a6bff1d451c0bd301fd52cbded58dd438f37c743
Ivan Dias <ivan.dias@exaud.com>
2021-11-18 10:55:17 +0000

    MSLI-1 - create IOS project

---
+++ My Series List/My Series List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Series List
+//
+//  Created by Ivan Dias on 18/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
