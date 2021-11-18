20414d5c7a6970f74a2e6108b22a8033328b3c72
Ivan Dias <ivan.dias@exaud.com>
2021-11-18 11:07:44 +0000

    MSLI-1 - create file and struct for series fetch mapping

---
+++ My Series List/My Series List/Model/SeriesModel.swift
@@ -0,0 +1,19 @@
+//
+//  SeriesModel.swift
+//  My Series List
+//
+//  Created by Ivan Dias on 18/11/2021.
+//
+
+import Foundation
+
+struct Results: Codable {
+    let results: [Series]
+}
+
+struct Series: Codable {
+    let name: String?
+    let overview: String?
+    let poster_path: String?
+    let vote_average: Double?
+}
