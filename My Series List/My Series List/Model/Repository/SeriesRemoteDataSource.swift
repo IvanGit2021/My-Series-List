2e28468b9aae212a0eb0d20a4fbe56572b6aacb5
Ivan Dias <ivan.dias@exaud.com>
2022-01-03 15:03:33 +0000

    MSLI-3 - change parameters to separate URL for reutilization

--- My Series List/My Series List/Model/Repository/SeriesRemoteDataSource.swift
+++ My Series List/My Series List/Model/Repository/SeriesRemoteDataSource.swift
@@ -9,11 +9,13 @@ import Foundation
 
 class SeriesRemoteDataSource {
     
-    let urlSearch = "https://api.themoviedb.org/3/search/movie?api_key=0fbc13d4f4bb3dcc23cb30a1a289d426&query="
-    let urlPopular = "https://api.themoviedb.org/3/movie/popular?api_key=0fbc13d4f4bb3dcc23cb30a1a289d426&language=en-US&page=1"
+    let apiUrl = "https://api.themoviedb.org/3/"
+    let apiParameters = "api_key=0fbc13d4f4bb3dcc23cb30a1a289d426"
     
     func getData(search: String, completionHandler: @escaping (Result<Api.Results, Error>) -> Void) {
-        let task = URLSession.shared.dataTask(with: URL (string: search == "" ? urlPopular : urlSearch + search )! , completionHandler: { data, response, error in
+        let searchUrl = apiUrl + "search/movie?query=" + search + "&" + apiParameters
+        let popularUrl = apiUrl + "movie/popular?language=en-US&page=1&" + apiParameters
+        let task = URLSession.shared.dataTask(with: URL (string: search == "" ? popularUrl : searchUrl )! , completionHandler: { data, response, error in
             if let error = error {
                 completionHandler(.failure(error))
             } else if let data = data {
