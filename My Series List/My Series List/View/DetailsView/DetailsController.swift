a95780fbe7f00cdef0265c4d437a0e193327e8a1
Ivan Dias <ivan.dias@exaud.com>
2022-01-05 18:07:12 +0000

    MSLI-4 - add data to layout components

--- My Series List/My Series List/View/DetailsView/DetailsController.swift
+++ My Series List/My Series List/View/DetailsView/DetailsController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class DetailsController: UIViewController {
 
@@ -20,12 +21,14 @@ class DetailsController: UIViewController {
     
     let detailsPresenter = DetailsPresenter()
     var id: Int32?
+    var details: Details?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         detailsPresenter.detailsView = self
         detailsPresenter.getDetails(id: id!)
     }
+    
     @IBAction func goToWebsite(_ sender: UIButton) {
         let homePage = details?.homePage == "" ? "https://www.netflix.com" : details?.homePage
         UIApplication.shared.open(URL(string: (homePage)!)! as URL)
@@ -35,5 +38,22 @@ class DetailsController: UIViewController {
 extension DetailsController: DetailsView {
     
     func listDetails(details: Details) {
+        self.details = details
+        DispatchQueue.main.async { [self] in
+            let imageUrl = URL(string: details.backdropPath == nil ? "" : "https://image.tmdb.org/t/p/w500" + details.backdropPath!)
+            let imageUrl2 = URL(string: details.posterPath == nil ? "" : "https://image.tmdb.org/t/p/w500" + details.posterPath!)
+            var allGenres = ""
+            for name in details.genres! {
+                allGenres += name.name! + " "
+            }
+            
+            detailsBackgroundImage.kf.setImage(with: imageUrl)
+            detailsFrontImage.kf.setImage(with: imageUrl2)
+            detailsTitle.text = details.name
+            detailsGenre.text = allGenres
+            detailsSeasons.text = details.numberOfSeasons?.description
+            detailsLanguage.text = details.originalLanguage?.uppercased()
+            detailsOverview.text = details.overView
+        }
     }
 }
