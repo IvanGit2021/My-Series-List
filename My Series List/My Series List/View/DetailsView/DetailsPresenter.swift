e38265e26e75631336ef2cdd2f8dabe675121baf
Ivan Dias <ivan.dias@exaud.com>
2022-01-06 20:20:53 +0000

    MSLI-4 - get details from coreData case no data from Api

--- My Series List/My Series List/View/DetailsView/DetailsPresenter.swift
+++ My Series List/My Series List/View/DetailsView/DetailsPresenter.swift
@@ -20,10 +20,28 @@ class DetailsPresenter {
     func getDetails(id: Int32) {
         seriesRepository.searchSeriesDetails(id: id) { [self] results in
             switch results {
-            case .failure(let error):
-                break
             case .success(let details):
                 detailsView?.listDetails(details: details)
+            case .failure(_):
+                seriesRepository.getSeries { results in
+                    switch results {
+                    case (.failure(_)):
+                        break
+                    case (.success(let seriesCoreData)):
+                        var count = 0
+                        for series in seriesCoreData {
+                            if series.id == id {
+                                let details = series
+                                detailsView?.listDetailsCoreData(details: details)
+                                count += 1
+                                break
+                            }
+                        }
+                        if count == 0 {
+                            detailsView?.showEmpty()
+                        }
+                    }
+                }
             }
         }
     }
